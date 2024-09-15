58cf546c3389b9d46c3130996e344f51f96cecbd
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2024-09-15 10:35:31 -0300

    Feat: tests upgradeable contract

--- multitoken-erc1155-upgradeable/test/Multitoken.test.ts
+++ multitoken-erc1155-upgradeable/test/Multitoken.test.ts
@@ -1,33 +1,30 @@
 import { loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import { ethers, upgrades } from "hardhat";
+import { Multitoken } from "../typechain-types";
 
 describe("Multitoken", function () {
   async function deployFixture() {
     const [owner, otherAccount] = await ethers.getSigners();
 
     const Multitoken = await ethers.getContractFactory("Multitoken");
-    const contract = await Multitoken.deploy();
+    const contract = await upgrades.deployProxy(Multitoken);
     const contractAddress = await contract.getAddress();
 
     return { contract, contractAddress, owner, otherAccount };
   }
 
-  it("should successfully mint a token and update balance and supply accordingly", async function () {
+  it("should successfully mint a token and update the balance", async function () {
     const { contract, owner } = await loadFixture(deployFixture);
 
     // Mint a token with the specified value
     await contract.mint(0, { value: ethers.parseEther("0.01") });
 
-    // Retrieve the updated balance and supply
+    // Retrieve the updated balance
     const balance = await contract.balanceOf(owner.address, 0);
-    const supply = await contract.currentSupply(0);
 
     // Verify that the balance has been updated correctly
     expect(balance).to.equal(1, "The balance after minting should be 1");
-
-    // Verify that the supply has been updated correctly
-    expect(supply).to.equal(49, "The supply after minting should be 49");
   });
 
   it("should revert when attempting to mint a non-existent token", async function () {
@@ -59,7 +56,7 @@ describe("Multitoken", function () {
       .to.be.revertedWith("Max supply reached");
   });
 
-  it("should successfully burn a token and update balance and supply accordingly", async function () {
+  it("should successfully burn a token and update the balance and supply", async function () {
     const { contract, owner } = await loadFixture(deployFixture);
 
     // Mint a token to the owner's address
@@ -68,15 +65,11 @@ describe("Multitoken", function () {
     // Burn the minted token
     await contract.burn(owner.address, 0, 1);
 
-    // Retrieve the updated balance and supply
+    // Retrieve the updated balance
     const balance = await contract.balanceOf(owner.address, 0);
-    const supply = await contract.currentSupply(0);
 
     // Verify that the balance is updated correctly after burning
     expect(balance).to.equal(0, "The balance after burning should be 0");
-
-    // Verify that the supply is updated correctly after burning
-    expect(supply).to.equal(49, "The supply after burning should be 49");
   });
 
   it("should allow an approved account to burn a token and update balance and supply accordingly", async function () {
@@ -93,18 +86,14 @@ describe("Multitoken", function () {
     expect(approved).to.equal(true, "The account should be approved to manage owner's tokens");
 
     // Connect to the contract as `otherAccount` and burn a token
-    const instance = contract.connect(otherAccount);
+    const instance = contract.connect(otherAccount) as Multitoken;
     await instance.burn(owner.address, 0, 1);
 
     // Retrieve the updated balance and supply
     const balance = await contract.balanceOf(owner.address, 0);
-    const supply = await contract.currentSupply(0);
 
     // Verify that the balance is updated correctly after burning
     expect(balance).to.equal(0, "The balance after burning by an approved account should be 0");
-
-    // Verify that the supply is updated correctly after burning
-    expect(supply).to.equal(49, "The supply after burning by an approved account should be 49");
   });
 
   it("should revert when attempting to burn more tokens than the balance", async function () {
@@ -122,14 +111,14 @@ describe("Multitoken", function () {
     await contract.mint(0, { value: ethers.parseEther("0.01") });
 
     // Connect to the contract as `otherAccount`
-    const instance = contract.connect(otherAccount);
+    const instance = contract.connect(otherAccount) as Multitoken;
 
     // Attempt to burn a token without having approval to do so
     await expect(instance.burn(owner.address, 0, 1))
       .to.be.revertedWithCustomError(contract, "ERC1155MissingApprovalForAll");
   });
 
-  it("should successfully transfer a token using safeTransferFrom and update balances and supply accordingly", async function () {
+  it("should successfully transfer a token using safeTransferFrom and update balances accordingly", async function () {
     const { contract, owner, otherAccount } = await loadFixture(deployFixture);
 
     // Mint a token to the owner's address
@@ -140,16 +129,12 @@ describe("Multitoken", function () {
 
     // Retrieve the updated balances for both addresses and the current supply
     const balances = await contract.balanceOfBatch([owner.address, otherAccount.address], [0, 0]);
-    const supply = await contract.currentSupply(0);
 
     // Verify that the owner's balance is updated correctly after the transfer
     expect(balances[0]).to.equal(0, "The owner's balance should be 0 after the transfer");
 
     // Verify that the recipient's balance is updated correctly after the transfer
     expect(balances[1]).to.equal(1, "The recipient's balance should be 1 after the transfer");
-
-    // Verify that the supply remains unchanged after the transfer
-    expect(supply).to.equal(49, "The supply should remain 49 after the transfer");
   });
 
   it("should emit a TransferSingle event when a token is transferred", async function () {
@@ -179,8 +164,6 @@ describe("Multitoken", function () {
       [owner.address, owner.address, otherAccount.address, otherAccount.address],
       [0, 1, 0, 1]
     );
-    const supply0 = await contract.currentSupply(0);
-    const supply1 = await contract.currentSupply(1);
 
     // Verify that the owner's balance for token type 0 is updated correctly after the transfer
     expect(balances[0]).to.equal(0, "The owner's balance for token type 0 should be 0 after the batch transfer");
@@ -193,12 +176,6 @@ describe("Multitoken", function () {
 
     // Verify that the recipient's balance for token type 1 is updated correctly after the transfer
     expect(balances[3]).to.equal(1, "The recipient's balance for token type 1 should be 1 after the batch transfer");
-
-    // Verify that the supply for token type 0 remains unchanged after the transfer
-    expect(supply0).to.equal(49, "The supply for token type 0 should remain 49 after the batch transfer");
-
-    // Verify that the supply for token type 1 remains unchanged after the transfer
-    expect(supply1).to.equal(49, "The supply for token type 1 should remain 49 after the batch transfer");
   });
 
   it("should emit a TransferBatch event with the correct parameters when multiple tokens are transferred", async function () {
@@ -220,7 +197,7 @@ describe("Multitoken", function () {
       );
   });
 
-  it("should successfully transfer a token using safeTransferFrom and update balances and supply accordingly", async function () {
+  it("should successfully transfer a token using safeTransferFrom and update balances accordingly", async function () {
     const { contract, owner, otherAccount } = await loadFixture(deployFixture);
 
     // Mint a token to the owner's address
@@ -230,21 +207,17 @@ describe("Multitoken", function () {
     await contract.setApprovalForAll(otherAccount.address, true);
 
     // Connect to the contract as `otherAccount` and perform the transfer
-    const instance = contract.connect(otherAccount);
+    const instance = contract.connect(otherAccount) as Multitoken;
     await instance.safeTransferFrom(owner.address, otherAccount.address, 0, 1, "0x00000000");
 
     // Retrieve the updated balances for both addresses and the current supply
     const balances = await contract.balanceOfBatch([owner.address, otherAccount.address], [0, 0]);
-    const supply = await contract.currentSupply(0);
 
     // Verify that the owner's balance for token type 0 is updated correctly after the transfer
     expect(balances[0]).to.equal(0, "The owner's balance for token type 0 should be 0 after the transfer");
 
     // Verify that the recipient's balance for token type 0 is updated correctly after the transfer
     expect(balances[1]).to.equal(1, "The recipient's balance for token type 0 should be 1 after the transfer");
-
-    // Verify that the supply for token type 0 remains unchanged after the transfer
-    expect(supply).to.equal(49, "The supply for token type 0 should remain 49 after the transfer");
   });
 
   it("should emit an ApprovalForAll event with the correct parameters when setting approval for all tokens", async function () {
@@ -279,7 +252,7 @@ describe("Multitoken", function () {
     await contract.mint(0, { value: ethers.parseEther("0.01") });
 
     // Connect to the contract as `otherAccount`
-    const instance = contract.connect(otherAccount);
+    const instance = contract.connect(otherAccount) as Multitoken;
 
     // Attempt to transfer a token from the owner to `otherAccount` without `otherAccount` having the necessary approval
     await expect(instance.safeTransferFrom(owner.address, otherAccount.address, 0, 1, "0x00000000"))
@@ -306,7 +279,7 @@ describe("Multitoken", function () {
     await contract.mint(1, { value: ethers.parseEther("0.01") });
 
     // Connect to the contract as `otherAccount`
-    const instance = contract.connect(otherAccount);
+    const instance = contract.connect(otherAccount) as Multitoken;
 
     // Attempt a batch transfer from `owner` to `otherAccount` without `otherAccount` having the necessary approval
     await expect(instance.safeBatchTransferFrom(owner.address, otherAccount.address, [0, 1], [1, 1], "0x00000000"))
@@ -327,7 +300,7 @@ describe("Multitoken", function () {
     const { contract, contractAddress, owner, otherAccount } = await loadFixture(deployFixture);
 
     // Connect to the contract as `otherAccount` and mint a token to trigger a deposit
-    const instance = contract.connect(otherAccount);
+    const instance = contract.connect(otherAccount) as Multitoken;
     await instance.mint(0, { value: ethers.parseEther("0.01") });
 
     // Check the contract's and owner's balance before the withdrawal
@@ -351,15 +324,15 @@ describe("Multitoken", function () {
     expect(ownerBalanceAfter).to.be.greaterThan(ownerBalanceBefore, "The owner's balance should increase after withdrawal");
   });
 
-  it("should revert with 'You do not have permission' when an unauthorized account attempts to withdraw funds", async function () {
+  it("should revert with 'OwnableUnauthorizedAccount' when an unauthorized account attempts to withdraw funds", async function () {
     const { contract, otherAccount } = await loadFixture(deployFixture);
 
     // Connect to the contract as `otherAccount`, who should not have permission to withdraw
-    const instance = contract.connect(otherAccount);
+    const instance = contract.connect(otherAccount) as Multitoken;
 
     // Attempt to perform the withdrawal from an unauthorized account
     await expect(instance.withdraw())
-      .to.be.revertedWith("You do not have permission");
+      .to.be.revertedWithCustomError(contract, "OwnableUnauthorizedAccount");
   });
 
   it("should return the correct URI for the token metadata", async function () {
