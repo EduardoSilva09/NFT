cbc8d4b31c054eb102ed60e0c3c4b47b1aa1ec5a
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2024-09-25 17:33:24 -0300

    Feat: Utils components

---
+++ openc/dapp/src/app/create/page.tsx
@@ -0,0 +1,10 @@
+import Footer from "@/components/footer";
+import Header from "@/components/header";
+
+function Create() {
+  return (<>
+    <Header />
+    <Footer />
+  </>)
+}
+export default Create;
