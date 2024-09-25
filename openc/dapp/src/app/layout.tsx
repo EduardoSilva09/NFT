ffa0a6a6f4bcd11f9bb993900a3c2dbfabdea501
EduardoSilva09 <silvafeitosaeduardo71@gmail.com>
2024-09-24 21:58:05 -0300

    Feat: Template NFT Origin

--- openc/dapp/src/app/layout.tsx
+++ openc/dapp/src/app/layout.tsx
@@ -25,6 +25,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+      </head>
       <body
         className={`text-gray-500 ${geistSans.variable} ${geistMono.variable} antialiased`}
       >
